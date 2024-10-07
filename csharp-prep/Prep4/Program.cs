089ac5f3d13b9555b2e3af8d09662e103147c7d6
Samuel-David Kelechi Imo <imo23002@byui.edu>
2024-10-06 21:45:50 -0600

    Finished Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int _number;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            _number = int.Parse(Console.ReadLine());
+            if (_number != 0)
+            {
+            numbers.Add(_number);
+            }
+        }  while(_number != 0);
+        int _sumNumbers = numbers.Sum();
+        double _averageNumbers = numbers.Average();
+        int _maxNumber = numbers.Max();
+        Console.WriteLine($"The sum is: {_sumNumbers}");
+        Console.WriteLine($"The average is: {_averageNumbers}");
+        Console.WriteLine($"The largest number is: {_maxNumber}");
     }
 }
