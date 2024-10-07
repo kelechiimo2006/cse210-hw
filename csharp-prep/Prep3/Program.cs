80ae9955318d15cb30d4c9c3e6d16550217c434f
Samuel-David Kelechi Imo <imo23002@byui.edu>
2024-10-06 20:12:36 -0600

    finishes prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
-}
+    {   Random randomGenerator = new Random();
+        int _magicNum = randomGenerator.Next(1, 100);
+        int _guessNumber;
+        do
+        {
+            string result = "";
+            Console.Write("What is your guess? ");
+            _guessNumber = int.Parse(Console.ReadLine());
+            if (_magicNum > _guessNumber)
+            {
+                result = "Higher";
+            }
+            else if (_magicNum < _guessNumber)
+            {
+                result = "Lower";
+            }
+            
+            Console.WriteLine($"{result}");
+
+        } while (_magicNum != _guessNumber);  
+        Console.WriteLine("You guessed right!");
+
+    
+    
+    }       
+} 
